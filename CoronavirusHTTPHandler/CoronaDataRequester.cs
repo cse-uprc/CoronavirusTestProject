6a841ad9d63f13ca46570110338ade3981e540bc
Jonathan Bender <jonathan.bender@rockets.utoledo.edu>
2020-03-28 14:26:48 -0400

    Modified the way to retrieve the list of cases since day one. Now accounts for deaths and recoveries.

--- CoronavirusHTTPHandler/CoronaDataRequester.cs
+++ CoronavirusHTTPHandler/CoronaDataRequester.cs
@@ -24,7 +24,7 @@ namespace CoronavirusWebHandler
             // For each country, join all of its cases together into a single list.
             CountrySlugs.ForEach(async cs =>
                 AllRelevantCases
-                .AddRange(await GetCasesPerCountry(cs))
+                .AddRange(await GetAllCasesPerCountry(cs))
             );
 
             return AllRelevantCases;
@@ -32,26 +32,46 @@ namespace CoronavirusWebHandler
 
         public static List<Country> GetSummary()
         {
-            string CountryListJSON = GetRequest("summary");
+            string CountryListJSON = GetRequest(COVID19_API_URL + "summary");
             var CountryListWrapper = JsonSerializer.Deserialize<SummaryWrapper>(CountryListJSON);
 
             return CountryListWrapper.Countries;
         }
 
-        private static async Task<List<Case>> GetCasesPerCountry(string CountrySlug)
+        // ----------------------------------------------------------------------
+
+        private static async Task<List<Case>> GetAllCasesPerCountry(string CountrySlug)
         {
-            const string DAYONE_GET_FORMAT = "dayone/country/{0}/status/confirmed";
-            string DayOneGet = String.Format(DAYONE_GET_FORMAT, CountrySlug);
+            // Format strings to be used below.
+            const string DAYONE_GET_CONFIRMED_FORMAT = "https://api.covid19api.com/dayone/country/{0}/status/confirmed";
+            const string DAYONE_GET_DEATHS_FORMAT = "https://api.covid19api.com/dayone/country/{0}/status/deaths";
+            const string DAYONE_GET_RECOVERED_FORMAT = "https://api.covid19api.com/dayone/country/{0}/status/recovered";
 
-            string SerializedResponse = await GetRequestAsync(DayOneGet);
+            // Prepare get requests by formatting strings
+            string ConfirmedCasesURL = String.Format(DAYONE_GET_CONFIRMED_FORMAT, CountrySlug);
+            string DeathsURL = String.Format(DAYONE_GET_DEATHS_FORMAT, CountrySlug);
+            string RecoveredURL = String.Format(DAYONE_GET_RECOVERED_FORMAT, CountrySlug);
 
-            return JsonSerializer.Deserialize<List<Case>>(SerializedResponse);
+            // Get all confirmed cases, deaths, and recoveries for the provided country by GET request
+            List<Case> ConfirmedList = JsonSerializer.Deserialize<List<Case>>(await GetRequestAsync(ConfirmedCasesURL));
+            List<Case> DeathsList = JsonSerializer.Deserialize<List<Case>>(await GetRequestAsync(DeathsURL));
+            List<Case> RecoveredList = JsonSerializer.Deserialize<List<Case>>(await GetRequestAsync(RecoveredURL));
+
+            // Merge these three lists into one singular list.
+            List<Case> AllRelevantCases = new List<Case>();
+            AllRelevantCases.AddRange(ConfirmedList);
+            AllRelevantCases.AddRange(DeathsList);
+            AllRelevantCases.AddRange(RecoveredList);
+
+            // Return this list.
+            return AllRelevantCases;
         }
 
-        private static string GetRequest(string EndpointName)
+        // ----------------------------------------------------------------
+
+        private static string GetRequest(string URL)
         // Sends a GET request to the specified endpoint and returns the result.
         {
-            string URL = URI(EndpointName);
             string EndpointResponseString;
 
             HttpWebRequest Request = (HttpWebRequest)WebRequest.Create(URL);
@@ -71,10 +91,9 @@ namespace CoronavirusWebHandler
             return EndpointResponseString;
         }
 
-        private static string PostRequest(string EndpointName, string JsonEncodedRequestBody)
+        private static string PostRequest(string URL, string JsonEncodedRequestBody)
         {
             byte[] Data = Encoding.UTF8.GetBytes(JsonEncodedRequestBody);
-            string URL = URI(EndpointName);
 
             string EndpointResponseString;
 
@@ -106,10 +125,9 @@ namespace CoronavirusWebHandler
             return EndpointResponseString;
         }
 
-        private static async Task<string> GetRequestAsync(string EndpointName)
+        private static async Task<string> GetRequestAsync(string URL)
         // Sends a GET request to the specified endpoint and returns the result.
         {
-            string URL = URI(EndpointName);
             string EndpointResponseString;
 
             HttpWebRequest Request = (HttpWebRequest)WebRequest.Create(URL);
@@ -129,10 +147,9 @@ namespace CoronavirusWebHandler
             return EndpointResponseString;
         }
 
-        private static async Task<string> PostRequestAsync(string EndpointName, string JsonEncodedRequestBody)
+        private static async Task<string> PostRequestAsync(string URL, string JsonEncodedRequestBody)
         {
             byte[] Data = Encoding.UTF8.GetBytes(JsonEncodedRequestBody);
-            string URL = URI(EndpointName);
 
             string EndpointResponseString;
 
@@ -162,11 +179,6 @@ namespace CoronavirusWebHandler
             }
 
             return EndpointResponseString;
-        }
-
-        private static string URI(string EndpointName)
-        {
-            return CoronaDataRequester.COVID19_API_URL + EndpointName;
         }
     }
 }
